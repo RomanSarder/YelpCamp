97aeb2c9236c9fe55bc82746a2fdafc593b2b52c
Роман <roman.sarder@yandex.ru>
2017-02-07 00:09:24 +0200

    Add mongoose Setup YelpCamp schema Sync routes with mongo

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express    = require('express'),
+	app        = express(),
+    bodyParser = require('body-parser'),
+    mongoose   = require('mongoose')
 
-var camps = [
-		{name: "Salmon Creek", image:"https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
-		{name: "Granite Hill", image:"https://farm7.staticflickr.com/6105/6381606819_df560e1a51.jpg"},
-		{name: "Mountain Goat's rest", image:"https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg"},
-		{name: "Salmon Creek", image:"https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
-		{name: "Granite Hill", image:"https://farm7.staticflickr.com/6105/6381606819_df560e1a51.jpg"},
-		{name: "Mountain Goat's rest", image:"https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg"},
-		{name: "Salmon Creek", image:"https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
-		{name: "Granite Hill", image:"https://farm7.staticflickr.com/6105/6381606819_df560e1a51.jpg"},
-		{name: "Mountain Goat's rest", image:"https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg"}
 
-	]
-
+mongoose.connect('mongodb://localhost/yelp_camp');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+var Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', function(req,res) {
 	res.render('landing');
 });
 
 app.get('/campgrounds', function(req,res) {
-	res.render('campgrounds', {camps: camps});
+	Campground.find({}, function(err,camps) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', {camps: camps});
+		}
+	}); 
 });
 
 app.get('/campgrounds/new', function(req,res) {
@@ -34,8 +36,13 @@ app.post('/campgrounds', function(req,res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCamp = {name: name, image:image};
-	camps.push(newCamp);
-	res.redirect('/campgrounds');
+	Campground.create(newCamp, function(err,newCamp) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect('/campgrounds');
+		}
+	});
 });
 
 app.listen(3000, function() {
