84e5e0a4683367c3d5f69368a7922ac7dbca842a
Роман <roman.sarder@yandex.ru>
2017-02-08 13:19:20 +0200

    Add show page Made separate file for camp model Add description

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var express    = require('express'),
 	app        = express(),
     bodyParser = require('body-parser'),
-    mongoose   = require('mongoose')
+    mongoose   = require('mongoose'),
+    Campground = require('./models/campground');
 
 
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String
-});
-var Campground = mongoose.model('Campground', campgroundSchema);
+// Campground.create({
+// 	name: "Granite Hill",
+// 	image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+// 	description: "Thit is a huge granite hill, no bathrooms. No water, Beautiful granite!"
+// }, function(err,camp) {
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(camp);
+// 	}
+// });
 
 app.get('/', function(req,res) {
 	res.render('landing');
@@ -23,7 +30,7 @@ app.get('/campgrounds', function(req,res) {
 		if (err) {
 			console.log(err);
 		} else {
-			res.render('campgrounds', {camps: camps});
+			res.render('index', {camps: camps});
 		}
 	}); 
 });
@@ -35,12 +42,23 @@ app.get('/campgrounds/new', function(req,res) {
 app.post('/campgrounds', function(req,res) {
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCamp = {name: name, image:image};
+	var desc = req.body.description;
+	var newCamp = {name: name, image:image, description: desc};
 	Campground.create(newCamp, function(err,newCamp) {
 		if(err) {
 			console.log(err);
 		} else {
 			res.redirect('/campgrounds');
+		}
+	});
+});
+
+app.get('/campgrounds/:id', function(req,res) {
+	Campground.findById(req.params.id, function(err, found) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render('show', {camp: found});
 		}
 	});
 });
