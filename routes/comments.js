1777911d141f107789c60185a41b5f75f48725ef
Роман <roman.sarder@yandex.ru>
2017-02-10 15:57:43 +0200

    Add Edit comment route Add Edit button Add Update comment route

--- routes/comments.js
+++ routes/comments.js
@@ -39,6 +39,26 @@ router.post('/', isLoggedIn, function(req,res) {
 	});
 });
 
+//EDIT
+router.get('/:comment_id/edit', function(req,res) {
+	Comment.findById(req.params.comment_id, function(err, found) {
+		if(err) {
+			res.redirect('back');
+		} else {
+			res.render('comments/edit', {campground_id: req.params.id, comment: found});;
+		}
+	});
+});
+
+router.put('/:comment_id', function(req,res) {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updated) {
+		if(err) {
+			res.redirect('back');
+		} else {
+			res.redirect('/campgrounds/' + req.params.id);
+		}
+	});
+});
 //MIDDLEWARE
 function isLoggedIn(req,res,next) {
 	if(req.isAuthenticated()) {
