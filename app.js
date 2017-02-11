41211afc421c7b98a5fff57c7681e26ece62ee3b
Роман <roman.sarder@yandex.ru>
2017-02-11 17:29:03 +0200

    Style login Style register Add req.path to figure out how to implement extra style

--- app.js
+++ app.js
@@ -46,12 +46,18 @@ app.use(function(req,res,next) {
 	res.locals.success = req.flash('success');
 	next();
 });
+//pass in path to view
+app.use(function(req, res, next) {
+  res.locals.current_path = req.path;
+  next();
+});
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen( 3000 || process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
+
 
